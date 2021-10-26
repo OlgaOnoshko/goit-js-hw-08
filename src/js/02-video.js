ca01cb9f96d2907db9c36fe265dc1e5e517eb215
OlgaOnoshko <onoshko.olga@gmail.com>
2021-10-27 01:53:20 +0300

    3rd with bug

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,34 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+console.log("localStorage")
 
 
 
 const iframe = document.querySelector('iframe');
 const iframePlayer = new Vimeo.Player(iframe);
-// const localStorage = 
 
-var onPlay = function(data) {
-    // data is an object containing properties specific to that event
-};
+localStorage.setItem("currentTime", 68890)
+// console.log(localStorage.getItem("videoplayer-current-time"))
+// const localStorage = ("videoplayer-current-time")
 
-player.on('play', onPlay);
 
-player.on('timeupdate', function(data) {
-    // data is an object containing properties specific to that event
-});
+// player.on('timeupdate', function(currentTime) {
+    // тут десь має бути тротл data is an object containing properties specific to that event
+// });
 
-console.log(player)
+// var onPlay = function(data) {
+//     // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
+
+// player.on('timeupdate', function(data) {
+//     // data is an object containing properties specific to that event
+// });
+
+console.log(throttle)
+console.log(localStorage)
+
+
 
