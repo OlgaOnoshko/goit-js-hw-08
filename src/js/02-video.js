5d9b55d5b78589e35ddace6fa37d1cc7de4a5a25
OlgaOnoshko <onoshko.olga@gmail.com>
2021-10-28 23:57:21 +0300

    3rd task corrections

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,34 +1,21 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-console.log("localStorage")
-
-
 
 const iframe = document.querySelector('iframe');
-const iframePlayer = new Vimeo.Player(iframe);
+const player = new Vimeo.Player(iframe);
 
-localStorage.setItem("currentTime", 68890)
-// console.log(localStorage.getItem("videoplayer-current-time"))
-// const localStorage = ("videoplayer-current-time")
+const LOCALSTARAGEKEY = "videoplayer-current-time"
+const setTime = localStorage.getItem(LOCALSTARAGEKEY)
 
+player.setCurrentTime(setTime)
 
-// player.on('timeupdate', function(currentTime) {
-    // тут десь має бути тротл data is an object containing properties specific to that event
-// });
+const onPlay = function(data) {
+    localStorage.setItem(LOCALSTARAGEKEY, data.seconds)
+};
 
-// var onPlay = function(data) {
-//     // data is an object containing properties specific to that event
-// };
+player.on('timeupdate', throttle(onPlay, 1000));
 
-// player.on('play', onPlay);
-
-// player.on('timeupdate', function(data) {
-//     // data is an object containing properties specific to that event
-// });
-
-console.log(throttle)
-console.log(localStorage)
 
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,19 +4,21 @@ const form = document.querySelector(".feedback-form")
 const email = document.querySelector(".feedback-form input")
 const message = document.querySelector(".feedback-form textarea")
 
-const formData = {}
+const formData = { email: "", message: ""}
 const STORAGEKEY = "feedback-form-state"
-
 
 form.addEventListener("submit", onFormSubmit)
 form.addEventListener("input", throttle(onFormInput, 500))
 
-function onFormInput(evt) {
-    formData[evt.target.name] = evt.target.value;
-    localStorage.setItem("STORAGEKEY", JSON.stringify(formData))
-}
-
 fillTextArea()
+
+function onFormInput(evt) {
+    // formData[evt.target.name] = evt.target.value;
+    formData.email = email.value
+    formData.message = message.value
+    localStorage.setItem("STORAGEKEY", JSON.stringify(formData))
+    // console.log(formData)
+}
 
 function onFormSubmit(evt) {
     evt.preventDefault()
@@ -27,23 +29,11 @@ function onFormSubmit(evt) {
 
 function fillTextArea() {
     const savedMessage = JSON.parse(localStorage.getItem("STORAGEKEY"))
-    console.log(savedMessage)
-
+    
     if (savedMessage) {
-        console.log(savedMessage)
+        // console.log(savedMessage)
         email.value = savedMessage.email
         message.value = savedMessage.message
     }
 }
 
-// email.addEventListener("input", onEmailInput)
-// message.addEventListener("input", throttle(onMessageInput, 500))
-// message.addEventListener("input", onMessageInput)
-
-// const STORAGEKEY = "input-message"
-// function onMessageInput(evt) {
-//     const messageText = evt.target.value
-//     console.log(messageText)
-
-//     localStorage.setItem("STORAGEKEY", messageText)
-// }
